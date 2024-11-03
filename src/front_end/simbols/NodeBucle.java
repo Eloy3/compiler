3394769b488ec275b7d107e4155a328f9b98444f
Eloy3 <fastt99@gmail.com>
2024-11-03 16:10:25 +0100

    fixed bug

--- src/front_end/simbols/NodeBucle.java
+++ src/front_end/simbols/NodeBucle.java
@@ -4,48 +4,26 @@ package front_end.simbols;
 
 public class NodeBucle extends NodeBase{
 
-    private NodeInicibucle inicibucle;
-    private NodeIncambit incambit;
     private NodeDecl_Variable decl_Variable;
-    private NodeEtiquetacond etiquetacond;
     private NodeCondicio condicio;
-    private NodeCondiciobot condiciobot;
     private NodeComportamentv1 comportamentv1;
 
     private NodeBlocf blocf;
-    private NodeRetornabucle retornabucle;
 
     //FOR
-    public NodeBucle(NodeInicibucle inicibucle, NodeIncambit incambit,
-            NodeDecl_Variable decl_Variable, NodeEtiquetacond etiquetacond, NodeCondicio condicio,
-            NodeCondiciobot condiciobot, NodeComportamentv1 comportamentv1, NodeBlocf blocf,
-            NodeRetornabucle retornabucle) {
+    public NodeBucle(NodeDecl_Variable decl_Variable, NodeCondicio condicio,NodeComportamentv1 comportamentv1, NodeBlocf blocf) {
         super("Buclefor", 0);
-        this.inicibucle = inicibucle;
-        this.incambit = incambit;
         this.decl_Variable = decl_Variable;
-        this.etiquetacond = etiquetacond;
         this.condicio = condicio;
-        this.condiciobot = condiciobot;
         this.comportamentv1 = comportamentv1;
         this.blocf = blocf;
-        this.retornabucle = retornabucle;
     }
 
     //WHILE
-    public NodeBucle(NodeInicibucle inicibucle, NodeEtiquetacond etiquetacond,
-                 NodeCondicio condicio, NodeCondiciobot condiciobot, 
-                 NodeIncambit incambit, NodeBlocf blocf, 
-                 NodeRetornabucle retornabucle)
-{
+    public NodeBucle(NodeCondicio condicio, NodeBlocf blocf){
         super("Bucle", 0);
-        this.inicibucle = inicibucle;
-        this.etiquetacond = etiquetacond;
         this.condicio = condicio;
-        this.condiciobot = condiciobot;
-        this.incambit = incambit;
         this.blocf = blocf;
-        this.retornabucle = retornabucle;
     }
 
     public void generateCodeWhile(){
@@ -60,7 +38,6 @@ public class NodeBucle extends NodeBase{
         ts.incAmbit();
         blocf.generateCode();
         retornabucle();
-        ts.decAmbit();
     }
     
     public void generateCodeFor(){
@@ -77,7 +54,6 @@ public class NodeBucle extends NodeBase{
         comportamentv1.generateCode();
         blocf.generateCode();
         retornabucle();
-        ts.decAmbit();
     }
 
     public static void inicibucle(){
